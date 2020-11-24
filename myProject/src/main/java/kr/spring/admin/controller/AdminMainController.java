3c4207c673e245eccc60eecf77329bbb36a1ac62
jein6 <jein6@LAPTOP-VVG9L9AG>
2020-11-24 10:03:23 +0900

    servlet변경 등 관리자 페이지 관련 파일 업뎃, 수정

--- myProject/src/main/java/kr/spring/admin/controller/AdminMainController.java
+++ myProject/src/main/java/kr/spring/admin/controller/AdminMainController.java
@@ -1,5 +1,22 @@
 package kr.spring.admin.controller;
 
-public class AdminMainController {
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.spring.musinfo.service.CommentsService;
+import kr.spring.musinfo.vo.CommentsVO;
+
+@Controller
+public class AdminMainController {
+	@RequestMapping("/admin/adminMain.do")
+	public String make(HttpServletResponse response) {
+		
+	
+		return "admin";
+		}
 }

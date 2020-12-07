aba2a4127a73ec7f3e77211496c239c14145ce92
ezen <ezen@DESKTOP-O03MT84>
2020-12-07 11:37:30 +0900

    에러

--- myProject/src/main/java/kr/spring/musinfo/dao/CommentsMapper.java
+++ myProject/src/main/java/kr/spring/musinfo/dao/CommentsMapper.java
@@ -12,7 +12,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import kr.spring.musinfo.vo.CommentsVO;
-
+   
 public interface CommentsMapper {
 	@Insert("insert into reviews(rev_num,mem_num,mus_num,rev_rate,review,rev_regdate) values(rev_seq.nextval,#{mem_num},#{mus_num},#{rev_rate},#{review},sysdate)")
 	public void insertComments(CommentsVO commentsVO);
